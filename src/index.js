e33aacd21e682dbc441d8cc571b86bbd62ff79ae
t4y3 <y.teraoka8@gmail.com>
2018-08-10 19:10:49 +0900

    Add: lint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { h, patch } from "superfine"
+import { h, patch } from 'superfine';
 import styles from './style.css';
 
 /**
@@ -25,7 +25,7 @@ export default class DomJsonTree {
    */
   _update() {
     this.node = patch(this.node, this._view(), this.container);
-  };
+  }
 
   /**
    * [_view description]
@@ -33,11 +33,15 @@ export default class DomJsonTree {
    */
   _view() {
     let template = [];
-    Object.keys(this.json).forEach((key) => {
+    Object.keys(this.json).forEach(key => {
       template.push(this._getObjectTemplate(key, this.json[key]));
     });
 
-    return h("div", { class: 'djt-Content', style: styles['Content'] }, template);
+    return h(
+      'div',
+      { class: 'djt-Content', style: styles['Content'] },
+      template
+    );
   }
 
   /**
@@ -49,49 +53,75 @@ export default class DomJsonTree {
   _getKeyTemplate(k, v) {
     if (this._isPrimitiveType(v)) {
       let type = typeof v;
-      let v2 = (type == 'string') ? `"${ v }"` : v.toString();
+      let v2 = type == 'string' ? `"${v}"` : v.toString();
 
-      return h(
-        "div",
-        { class: 'djt-Property', style: styles['Property'] },
-        [
-          h("span", { class: 'djt-Property_Key', style: styles['Property_Key'] }, k),
-          h("span", { class: 'djt-Property_Colon', style: styles['Property_Colon'] }, ': '),
-          h("span", { class: 'djt-Property_Type', style: styles[`Property_Type-${ type }`] }, v2),
-        ]
-      );
+      return h('div', { class: 'djt-Property', style: styles['Property'] }, [
+        h(
+          'span',
+          { class: 'djt-Property_Key', style: styles['Property_Key'] },
+          k
+        ),
+        h(
+          'span',
+          { class: 'djt-Property_Colon', style: styles['Property_Colon'] },
+          ': '
+        ),
+        h(
+          'span',
+          {
+            class: 'djt-Property_Type',
+            style: styles[`Property_Type-${type}`]
+          },
+          v2
+        )
+      ]);
     } else {
       // TODO: 畳んだ状態でも見えるように
-      let type = (typeof v.length === 'number') ? `Array[${ v.length }]`: 'Object';
+      let type = typeof v.length === 'number' ? `Array[${v.length}]` : 'Object';
       return h(
-        "div",
+        'div',
         {
           class: 'djt-Property',
           style: styles['Property'],
           'data-djt-toggle': 'open',
-          onclick: (e) => {
-
-            let target = e.target.classList.contains('djt-Property') ? e.target : e.target.parentNode;
+          onclick: e => {
+            let target = e.target.classList.contains('djt-Property')
+              ? e.target
+              : e.target.parentNode;
 
             if (target.dataset.djtToggle == 'open') {
               target.dataset.djtToggle = 'close';
               target.nextElementSibling.style.display = 'none';
-              target.parentNode.querySelector('.djt-Arrow').style.transform = 'none';
+              target.parentNode.querySelector('.djt-Arrow').style.transform =
+                'none';
             } else {
               target.dataset.djtToggle = 'open';
               target.nextElementSibling.style.display = 'block';
-              target.parentNode.querySelector('.djt-Arrow').style.transform = 'rotate(90deg)';
+              target.parentNode.querySelector('.djt-Arrow').style.transform =
+                'rotate(90deg)';
             }
           }
         },
         [
-          h("span", { class: 'djt-Property_Key', style: styles['Property_Key'] }, k),
-          h("span", { class: 'djt-Property_Colon', style: styles['Property_Colon'] }, ': '),
-          h("span", { class: 'djt-Property_Type', style: styles['Property_Type'] }, type),
+          h(
+            'span',
+            { class: 'djt-Property_Key', style: styles['Property_Key'] },
+            k
+          ),
+          h(
+            'span',
+            { class: 'djt-Property_Colon', style: styles['Property_Colon'] },
+            ': '
+          ),
+          h(
+            'span',
+            { class: 'djt-Property_Type', style: styles['Property_Type'] },
+            type
+          )
         ]
       );
     }
-  };
+  }
 
   /**
    * [_getValueTemplate description]
@@ -100,12 +130,12 @@ export default class DomJsonTree {
    */
   _getValueTemplate(v) {
     let template = [];
-    Object.keys(v).forEach((key) => {
+    Object.keys(v).forEach(key => {
       template.push(this._getObjectTemplate(key, v[key]));
     });
 
-    return h("div", { class: 'djt-Value', style: styles['Value'] }, template);
-  };
+    return h('div', { class: 'djt-Value', style: styles['Value'] }, template);
+  }
 
   /**
    * [_getObjectTemplate description]
@@ -119,13 +149,19 @@ export default class DomJsonTree {
     if (this._isPrimitiveType(v)) {
       template.push(this._getKeyTemplate(k, v));
     } else {
-      template.push(h("span", { class: 'djt-Arrow', style: styles['Arrow'] }, ''));
+      template.push(
+        h('span', { class: 'djt-Arrow', style: styles['Arrow'] }, '')
+      );
       template.push(this._getKeyTemplate(k, v));
       template.push(this._getValueTemplate(v));
     }
 
-    return h("div", { key: k, class: 'djt-object', style: styles['Object'] }, template);
-  };
+    return h(
+      'div',
+      { key: k, class: 'djt-object', style: styles['Object'] },
+      template
+    );
+  }
 
   /**
    * [_isPrimitiveType description]
